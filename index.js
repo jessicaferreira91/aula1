31e97cd46c5d98e88d483a8f874f8b11d187c799
Jessica Ferreira <jeporsh0@gmail.com>
2024-09-26 21:47:00 -0300

    aprendendo a comentar sobre códigos meu primeiro JS

---
+++ index.js
@@ -0,0 +1,13 @@
+// isso é um comentario de uma linha 
+/*
+e um comentario 
+multi linha
+*/
+//alert()"olha o js aqui")
+
+
+alert ('seja bem vindo' + nome + '!')
+
+
+console.log('imprimindo no console do servidor')
+let nome = prompt ("qual é o seu nome?")
